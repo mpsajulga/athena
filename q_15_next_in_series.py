daacf8aa9da94266522c9e8b42e1597903713521
mpsajulga <mychalphillipsajulga@gmail.com>
2023-03-16 10:35:10 +0800

    Answered question number 15

---
+++ q_15_next_in_series.py
@@ -0,0 +1,7 @@
+print(f"What number (X) comes next in the series (17, 9, 18, 7, 19, 5, 20, X)")
+
+print("Pattern for 1st > 3rd > 5th > 7th \n17 > 18 > 19 > 20 \nAll are plus ones \nWhile pattern for 2nd > 4th > 6th \n9 > 7 > 5 \nAll are subtracted by twos \nTherefore...")
+
+next_in_line = 5-2
+
+print(f"The number next in the series is {next_in_line}")
