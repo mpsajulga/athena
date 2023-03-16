460f1db4fe9a1fd8b9eb49c874e1740d3ca524d2
mpsajulga <mychalphillipsajulga@gmail.com>
2023-03-16 13:29:42 +0800

    Answered Q#35 about figuring out the participant percentage.

---
+++ q_35_participant_percentage.py
@@ -0,0 +1,12 @@
+print("Last year, about 2,400 people participated in a local event. This year, about 3,200 people participated. What was the approximate percent increase in participation?")
+
+initial_participants = 2400
+subsequent_participants = 3200
+
+difference_between_the_participants = subsequent_participants-initial_participants
+
+print(f"The difference between the participants {difference_between_the_participants}")
+
+percentage = (difference_between_the_participants/initial_participants)*100
+
+print(f"There has been an approximate {percentage} percent increase.")
