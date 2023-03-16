7f2b861eedc0eaa497195b5a5a64167c33afcd9e
mpsajulga <mychalphillipsajulga@gmail.com>
2023-03-16 10:59:48 +0800

    Answered question number 16

---
+++ q_16_next_in_series_2.py
@@ -0,0 +1,6 @@
+print("Choose the number (X) that should come next in this series (6 9 14 21 X).")
+
+print("Pattern for 1st > 2nd > 3rd > 4th \n6 > 9 > 14 > 21 \nAll are increments of +3, +5, and +7 \nWith a constant +3 for each increment. \nTherefore, the last one should be 7+2=9. Which would then be added to 21 \nTherefore...")
+
+x= 21+9
+print(x)
